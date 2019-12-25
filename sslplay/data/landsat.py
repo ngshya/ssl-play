b07f0babd245b062e3423a838f3e2118a898525d
ngshya <ngshya@gmail.com>
2019-12-25 21:14:14 +0100

    Added the test set to the landsat.

--- sslplay/data/landsat.py
+++ sslplay/data/landsat.py
@@ -1,5 +1,6 @@
-from pandas import read_csv
+from pandas import read_csv, concat
 import numpy as np
+import glob
 from sslplay.utils.lut_split import lut_split
 
 class DataLandsat:
@@ -9,8 +10,10 @@ class DataLandsat:
         self.name = "LANDSAT"
 
 
-    def load(self, path="data/landsat/sat.trn"):
-        self.data = read_csv(path, header=None, sep=" ")
+    def load(self, path="data/landsat/sat.*"):
+        self.lst_all_files = glob.glob(path)
+        self.data = (read_csv(f, header=None, sep=" ") for f in self.lst_all_files)
+        self.data = concat(self.data, ignore_index=True)
         self.target = np.array(self.data.iloc[:, 36].astype("category").cat.codes.values)
         self.data = self.data.iloc[:, 0:36]
         
