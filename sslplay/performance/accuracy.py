add998260e7c96ea10f733cf0f56868f7bc82006
ngshya <ngshya@gmail.com>
2019-12-27 21:11:07 +0100

    small update

--- sslplay/performance/accuracy.py
+++ sslplay/performance/accuracy.py
@@ -6,5 +6,7 @@ def accuracy(y_real, y_pred):
     y_pred = y_pred.argmax(axis=1)
     out = sum(y_real == y_pred) / len(y_real)
     baseline = sum(y_real == np.random.choice(y_real, size=len(y_real), replace=True)) / len(y_real)
-    logging.debug("ACCURACY: " + str(round(out, 4)) + " | Baseline: " + str(round(baseline, 4))) 
+
+    logging.debug("ACCURACY: " + str(round(out, 4)) + " | Baseline: " + str(round(baseline, 4)))
+ 
     return out
