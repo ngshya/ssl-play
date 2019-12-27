feb9ccbd60a5c793d061a7b7445ab8e96fdaf0ba
ngshya <ngshya@gmail.com>
2019-12-28 00:14:12 +0100

    added setup and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ssl-play
+
+[![Build Status](https://travis-ci.org/ngshya/ssl-play.svg?branch=master)](https://travis-ci.org/ngshya/ssl-play) [![Coverage Status](https://coveralls.io/repos/github/ngshya/ssl-play/badge.svg?branch=master)](https://coveralls.io/github/ngshya/ssl-play?branch=master)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='sslplay',
+    url='https://github.com/ngshya/ssl-play',
+    author='ngshya',
+    author_email='ngshya@gmail.com',
+    packages=['sslplay', 'sslplay.data', 'sslplay.model', 'sslplay.performance', 'sslplay.utils'],
+    install_requires=required,
+    version='1.1.1',
+    license='proprietary',
+    description='',
+    long_description=open('README.md').read(),
+    package_data={
+        'sslplay': ["*"],
+        'notebooks': ['*'],
+        'scripts': ['*']
+    }
+)
