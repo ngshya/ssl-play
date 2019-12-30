975d1df9820b846476113149e3e122a95f7fcdeb
ngshya <ngshya@gmail.com>
2019-12-30 11:17:27 +0100

    run GPU models firstly and then others

--- scripts/ssl-play-batch.py
+++ scripts/ssl-play-batch.py
@@ -5,7 +5,7 @@ import os
 from pathlib import Path
 
 array_perc_u = [40, 45, 50, 55, 60, 65, 70, 75, 79, 79.5, 79.9]
-array_class_model = ["rf", "nn", "krf", "ln", "ls"]
+array_class_model = ["nn", "ln", "rf", "krf", "ls"]
 array_class_data = [
     "spambase", 
     "creditcard", 
@@ -18,9 +18,9 @@ array_class_data = [
 
 if __name__ == "__main__":
 
-    for data in array_class_data: 
-        for punla in array_perc_u:
-            for model in array_class_model:
+    for model in array_class_model:
+        for data in array_class_data: 
+            for punla in array_perc_u:
 
                 punla = round(punla, 2)
                 plabe = round(80-punla, 2)
@@ -48,8 +48,8 @@ if __name__ == "__main__":
                     
                     cpu_available = 100 - float(subprocess.check_output("top -bn2 | grep 'Cpu(s)' | awk END'{print $2}'", shell=True).decode('ascii').replace("\n", "").replace(",", "."))
                     ram_available = float(subprocess.check_output("free | grep Mem | awk '{print $7/$2 * 100.0}'", shell=True).decode('ascii').replace("\n", ""))
-                    gpu_available = 100 - float(subprocess.check_output("nvidia-smi --format=csv,noheader --query-gpu=utilization.gpu", shell=True).decode('ascii').replace(" %\n", ""))
-                    gpu_mem_available = 100 - float(subprocess.check_output("nvidia-smi --format=csv,noheader --query-gpu=utilization.memory", shell=True).decode('ascii').replace(" %\n", ""))
+                    gpu_available = 100 - float(subprocess.check_output("nvidia-smi --format=csv,noheader --query-gpu=utilization.gpu | awk END'{print $1}'", shell=True).decode('ascii').replace(" %\n", ""))
+                    gpu_mem_available = 100 - float(subprocess.check_output("nvidia-smi --format=csv,noheader --query-gpu=utilization.memory | awk END'{print $1}'", shell=True).decode('ascii').replace(" %\n", ""))
 
                     print(
                         "AVAILABLE: CPU " + str(cpu_available) + 
@@ -60,13 +60,17 @@ if __name__ == "__main__":
 
                     if (cpu_available >= 20) & (ram_available >= 20):
                         if model in ["nn", "ln"]:
-                            if (gpu_available >= 70) & (gpu_mem_available >= 70):
+                            if (gpu_available >= 50) & (gpu_mem_available >= 50):
                                 break
                         else:
                             break
 
                     time.sleep(np.random.randint(20, 30))
 
+                if model in ["nn", "ln"]:
+                    str_background = ""
+                else:
+                    str_background = "&"
 
                 os.system(
                     "python scripts/ssl-play.py" + " " +
@@ -78,7 +82,7 @@ if __name__ == "__main__":
                     "--folds=" + str(5) + " " + 
                     "--samples=" + str(30) + " " +
                     "--outfolder=" + "outputs/batch_run_output/" + " " +
-                    "--seed=" + "1102" " " + 
-                    "&"
+                    "--seed=" + "1102" + " " + 
+                    str_background
                 )
 
