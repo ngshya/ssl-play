2297ce4ca4538c16e55e394d965662fd42afa414
ngshya <ngshya@gmail.com>
2019-12-30 01:21:51 +0100

    used subprocess instead of os.system

--- scripts/ssl-play-batch.py
+++ scripts/ssl-play-batch.py
@@ -1,7 +1,7 @@
-import os
-import psutil
 import time
 import numpy as np
+import subprocess
+import os
 
 array_perc_u = [40, 45, 50, 55, 60, 65, 70, 75, 79, 79.5, 79.9]
 array_class_model = ["rf", "nn", "krf", "ln", "ls"]
@@ -21,13 +21,22 @@ if __name__ == "__main__":
         for model in array_class_model:
             for punla in array_perc_u:
                 
-                time.sleep(np.random.randint(0, 120))
+                time.sleep(np.random.randint(0, 7))
 
                 while True:
-                    cpu_available = 100-psutil.cpu_percent()
-                    ram_available = psutil.virtual_memory().available /  psutil.virtual_memory().total * 100
-                    gpu_available = 100 - float(os.system("nvidia-smi --format=csv,noheader --query-gpu=utilization.gpu"))
-                    gpu_mem_available = 100 - float(os.system("nvidia-smi --format=csv,noheader --query-gpu=utilization.memory"))
+                    
+                    cpu_available = 100 - float(subprocess.check_output("top -bn2 | grep 'Cpu(s)' | awk END'{print $2}'", shell=True).decode('ascii').replace("\n", "").replace(",", "."))
+                    ram_available = float(subprocess.check_output("free | grep Mem | awk '{print $7/$2 * 100.0}'", shell=True).decode('ascii').replace("\n", ""))
+                    gpu_available = 100 - float(subprocess.check_output("nvidia-smi --format=csv,noheader --query-gpu=utilization.gpu", shell=True).decode('ascii').replace(" %\n", ""))
+                    gpu_mem_available = 100 - float(subprocess.check_output("nvidia-smi --format=csv,noheader --query-gpu=utilization.memory", shell=True).decode('ascii').replace(" %\n", ""))
+
+                    print(
+                        "AVAILABLE: CPU " + str(cpu_available) + 
+                        " RAM " + str(ram_available) + 
+                        " GPU " + str(gpu_available) + 
+                        " GPU-M " + str(gpu_mem_available)
+                    )
+
                     if (cpu_available >= 20) & (ram_available >= 20):
                         if model in ["nn", "ln"]:
                             if (gpu_available >= 33) & (gpu_mem_available >= 33):
